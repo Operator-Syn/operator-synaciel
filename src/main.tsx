5f698731fa66f6d121d39724a23affdbf96efce8
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-24 06:59:21 +0800

    Update reviewed repository file (main.tsx).

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'bootstrap/dist/css/bootstrap.css'; 
-import 'bootstrap/js/dist/collapse';
-import './index.css';
-import App from './App.tsx';
-import GlobalHeadManager from './components/globalHeadManager/GlobalHeadManager.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/app.css";
+import App from "./App.tsx";
+import GlobalHeadManager from "./components/globalHeadManager/GlobalHeadManager.tsx";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     {/* 3. Wrap your app with the Provider */}
     <QueryClientProvider client={queryClient}>
