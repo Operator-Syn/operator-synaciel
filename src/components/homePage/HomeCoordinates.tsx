103f782c6825076f0ae4dadca49290d10e83d168
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-26 01:30:26 +0800

    Update homepage coordinate presentation.

--- src/components/homePage/HomeCoordinates.tsx
+++ src/components/homePage/HomeCoordinates.tsx
@@ -1,10 +1,5 @@
 import PointerCoordinates from "../pointerCoordinates/PointerCoordinates";
-import type { HomeSectionIndex } from "./useHomepageMotion";
 
-interface HomeCoordinatesProps {
-  activeSection: HomeSectionIndex;
-}
-
-export default function HomeCoordinates({ activeSection }: HomeCoordinatesProps) {
-  return <PointerCoordinates activeSection={activeSection} className="homepage-coordinates" />;
+export default function HomeCoordinates() {
+  return <PointerCoordinates activeSection={0} className="homepage-coordinates" />;
 }
