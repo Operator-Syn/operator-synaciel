87738de3832f618dc35844ae3706d2de90ada617
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-27 09:45:25 +0800

    Update src/App.tsx with the reviewed working-tree changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
 import SnippetDocument from "./components/pages/snippetsPage/SnippetDocument";
+import PageTransition from "./components/pageTransition/PageTransition";
 import QuickNavigation from "./components/quickNavigation/QuickNavigation";
 import { brandName, navLinks as NavLinks, routes } from "./data/NavLinks.types";
 
 export default function App() {
   return (
-    <Fragment>
+    <PageTransition>
       <NavBar brandName={brandName} links={NavLinks} />
 
       <div className="app-shell">
@@ -24,6 +24,6 @@ export default function App() {
       </div>
 
       <QuickNavigation />
-    </Fragment>
+    </PageTransition>
   );
 }
