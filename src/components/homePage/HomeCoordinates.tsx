a4d367348d14590cd79a28f8268d893ab06e2296
Operator-Syn <johnronanbeira100404@gmail.com>
2026-08-28 09:38:15 +0800

    Relocate HomeCoordinates.tsx into its owning workspace.

--- src/components/homePage/HomeCoordinates.tsx
+++
@@ -1,5 +0,0 @@
-import PointerCoordinates from "../pointerCoordinates/PointerCoordinates";
-
-export default function HomeCoordinates() {
-  return <PointerCoordinates activeSection={0} className="homepage-coordinates" />;
-}
